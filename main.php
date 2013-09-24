a57607b3501e69dc4a19c5e7ede11daf289e4eae
Nancy Griffeth <nancy.griffeth@lehman.cuny.edu>
2013-09-24 16:27:23 -0400

    Finally fixed date.

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <h1>Nancy's Super Web Site</h1>
 
 <?php
-date_default_timezone_set("America/New York");
-echo "Order processed on ".date('H:i jS F Y');
+date_default_timezone_set("America/New_York");
+echo date('H:i, jS F Y');
 ?>
