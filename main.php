d0688f4367d0ba10cfedb21ccec678efcdad304d
Nancy Griffeth <nancy.griffeth@lehman.cuny.edu>
2013-09-24 16:59:39 -0400

    Add another jpeg of php generated page.

--- main.php
+++ main.php
@@ -1,6 +1,14 @@
 <h1>Nancy's Super Web Site</h1>
 
 <?php
+$yourname = $_POST['name'];
+$password=$_POST['password'];
+echo "<p>";
 date_default_timezone_set("America/New_York");
 echo "Order processed at".date('H:i, jS F Y');
+echo "<p>You are ";
+echo $yourname;
+echo "</p><p>Your password is ";
+echo $password;
+echo "</p>";
 ?>
