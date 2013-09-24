03ba425da0368573550b792bb22cf9904ea5d9ea
Nancy Griffeth <nancy.griffeth@lehman.cuny.edu>
2013-09-24 16:15:33 -0400

    Changed to 'America/New York'

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <h1>Nancy's Super Web Site</h1>
 
 <?php
-date_default_timezone_set("Americas/New York");
+date_default_timezone_set("America/New York");
 echo "Order processed on ".date('H:i jS F Y');
 ?>
